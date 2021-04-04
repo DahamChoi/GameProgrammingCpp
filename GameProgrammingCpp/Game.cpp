f7d569d63f7a320d8d5de6892a0efe674073b067
DahamChoi <rorigu32@gmail.com>
2021-04-04 23:08:04 +0900

    ch.1

--- GameProgrammingCpp/Game.cpp
+++ GameProgrammingCpp/Game.cpp
@@ -5,6 +5,7 @@ Game::Game()
 {
     IsRunning = true;
     Window = nullptr;
+    Renderer = nullptr;
 }
 
 bool Game::Initialize()
@@ -29,6 +30,17 @@ bool Game::Initialize()
         SDL_Log("Faield to create window: %s", SDL_GetError());
         return false;
     }
+
+    Renderer = SDL_CreateRenderer(
+        Window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if (!Renderer)
+    {
+        SDL_Log("Faield to create Renderer: %s", SDL_GetError());
+        return false;
+    }
     
     return true;
 }
@@ -46,17 +58,139 @@ void Game::RunLoop()
 void Game::Shutdown()
 {
     SDL_DestroyWindow(Window);
+    SDL_DestroyRenderer(Renderer);
     SDL_Quit();
 }
 
 void Game::ProcessInput()
 {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            IsRunning = false;
+            break;
+        }
+    }
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_ESCAPE])
+    {
+        IsRunning = false;
+    }
+
+    PaddleDirectionY = 0;
+    if (state[SDL_SCANCODE_W])
+    {
+        PaddleDirectionY -= 1;
+    }
+    if (state[SDL_SCANCODE_S])
+    {
+        PaddleDirectionY += 1;
+    }
 }
 
 void Game::UpdateGame()
 {
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), TicksCount + 16))
+        ;
+
+    float deltaTime = (SDL_GetTicks() - TicksCount) / 1000.f;
+    TicksCount = SDL_GetTicks();
+
+    if (deltaTime > 0.05f)
+        deltaTime = 0.05f;
+
+    if (PaddleDirectionY < 0.f)
+    {
+        if (PaddlePos.y - PaddleHeight / 2 > Thickness)
+            PaddlePos.y = PaddlePos.y + PaddleDirectionY * 300.f * deltaTime;
+    }
+    else if (PaddleDirectionY > 0.f)
+    {
+        if (PaddlePos.y + PaddleHeight / 2 < 768 - Thickness)
+            PaddlePos.y = PaddlePos.y + PaddleDirectionY * 300.f * deltaTime;
+    }
+
+    BallPos.x = BallPos.x + BallVel.x * 300.f * deltaTime;
+    BallPos.y = BallPos.y + BallVel.y * 300.f * deltaTime;
+
+    if (BallPos.y - (Thickness / 2) < Thickness && BallVel.y < 0.f)
+    {
+        BallVel.y *= -1.f;
+    }
+
+    if (BallPos.y + (Thickness / 2) > 768 - Thickness && BallVel.y > 0.f)
+    {
+        BallVel.y *= -1.f;
+    }
+
+    if (BallPos.x + (Thickness / 2) > 1024 - Thickness && BallVel.x > 0.f)
+    {
+        BallVel.x *= -1.f;
+    }
+
+    if ((BallPos.y + (Thickness / 2) > PaddlePos.y - PaddleHeight &&
+        BallPos.y - (Thickness / 2) < PaddlePos.y + PaddleHeight) &&
+        BallPos.x - (Thickness / 2) < PaddlePos.x + Thickness &&
+        BallVel.x < 0.f)
+    {
+        BallVel.y *= -1.f;
+        BallVel.x *= -1.f;
+    }
 }
 
 void Game::GenerateOutput()
 {
+    // Buffer Clear
+    SDL_SetRenderDrawColor(Renderer, 0, 0, 255, 255);
+    SDL_RenderClear(Renderer);
+
+    // Draw Game Buffer
+    static SDL_Rect TopWall{
+        0,
+        0,
+        1024,
+        Thickness
+    };
+
+    static SDL_Rect BottomWall{
+        0,
+        768 - Thickness,
+        1024,
+        Thickness
+    };
+
+    static SDL_Rect RightWall{
+        1024 - Thickness,
+        0,
+        Thickness,
+        768
+    };
+
+    SDL_Rect Ball{
+        static_cast<int>(BallPos.x - Thickness / 2),
+        static_cast<int>(BallPos.y - Thickness / 2),
+        Thickness,
+        Thickness
+    };
+
+    SDL_Rect Paddle{
+        static_cast<int>(PaddlePos.x - Thickness / 2),
+        static_cast<int>(PaddlePos.y - PaddleHeight / 2),
+        Thickness,
+        PaddleHeight
+    };
+
+    SDL_SetRenderDrawColor(Renderer, 128, 128, 255, 255);
+    SDL_RenderFillRect(Renderer, &TopWall);
+    SDL_RenderFillRect(Renderer, &BottomWall);
+    SDL_RenderFillRect(Renderer, &RightWall);
+    SDL_RenderFillRect(Renderer, &Ball);
+    SDL_RenderFillRect(Renderer, &Paddle);
+
+    // Swap Buffer
+    SDL_RenderPresent(Renderer);
 }
--- GameProgrammingCpp/Game.h
+++ GameProgrammingCpp/Game.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Vector2.h"
+
 class Game
 {
 public:
@@ -15,6 +17,18 @@ private:
 	void GenerateOutput();
 
 	class SDL_Window* Window;
+	class SDL_Renderer* Renderer;
 	bool IsRunning;
+
+	Vector2 BallPos{ 1024.f / 2.f, 768.f / 2.f };
+	Vector2 BallVel{ 1.f, 1.f };
+
+	Vector2 PaddlePos{ 30.f, 768.f / 2.f };
+	int PaddleDirectionY{ 0 };
+
+	unsigned int TicksCount{ 0 };
+
+	const static int Thickness{ 15 };
+	const static int PaddleHeight{ 75 };
 };
 
